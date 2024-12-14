c83ae38ad5d6e1d025900edabced5fc9e3cab059
ao donovan <a.o.donovan.music@gmail.com>
2024-12-14 14:52:52 -0500

    safety commit

--- src/main/java/com/example/SocialMediaApp.java
+++ src/main/java/com/example/SocialMediaApp.java
@@ -5,12 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-/**
- * This is a class that is used to run your application.
- *
- * DO NOT CHANGE ANYTHING IN THIS CLASS
- *
- */
 @SpringBootApplication
 public class SocialMediaApp {
     /**
